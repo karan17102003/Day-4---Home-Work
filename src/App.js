5d10c653a01574f23eb907eeec5652b9eb38f9a0
karan17102003 <karanmehra17102003@gmail.com>
2024-05-04 21:05:05 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,139 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='contaner1'>
+        <div className='div img-text'>
+          <img src='https://tse2.mm.bing.net/th?id=OIP.p5aepZh8OpbpDE8YlmVtWgAAAA&pid=Api&P=0&h=220'/>
+          <div>
+          <h1>JESSICA GREENE</h1>
+          <h2>Freelance Writer</h2>
+          </div>
+
+
+
+        </div>
+        <div className='div'>
+          <p>i specialize in writing long-form blog content. case studies, e-books. and white papers for b2bs. tech startups. and marketing agencies.</p>
+          <p>Email: myemail@gmail.com</p>
+          <p>website: myWebsite.com</p>
+          
+        </div>
+      </div>
+      <div className='contaner1'>
+        <img src='https://tse3.mm.bing.net/th?id=OIP.gRnZe5dam6IlPANYdwo4EwHaHa&pid=Api&P=0&h=220'></img>
+        <div>
+          <div className='contaner2-text'>
+            <h4>Present</h4>
+            <p>Jessica Greene Marketing. Freelance Writer</p>
+          </div>
+          <div className='contaner2-text'>
+            <h4>2016</h4>
+            <p>Jessica Greene Marketing. Freelance Writer</p>
+          </div>
+          <div className='contaner2-text'>
+            <h4>2013-2015</h4>
+            <p>Jessica Greene Marketing. Freelance Writer</p>
+          </div>
+        </div>
+      </div>
+      <div className='contaner1'>
+        <div className='div img-text'>
+          <img src='https://tse2.mm.bing.net/th?id=OIP.cKt8WN9rQJljnSk7KLfpugHaHa&pid=Api&P=0&h=220'/>
+          <div className='man'>
+          <div className ='skills'>
+         
+          <div>
+            Editing
+          </div>
+          <div className='stare'>
+            <div className='satre-skills'>
+
+            </div>
+            <div className='satre-skills'>
+
+            </div>
+            <div className='satre-skills'>
+
+            </div>
+            <div className='satre-skills'>
+
+            
+          </div>
+          </div>
+          <div>
+           SEO
+          </div>
+          <div className='stare'>
+            <div className='satre-skills'>
+
+            </div>
+            <div className='satre-skills'>
+
+            </div>
+            <div className='satre-skills'>
+
+            </div>
+            <div className='satre-skills'>
+
+            
+          </div>
+          </div>
+          <div>
+            Writing
+          </div>
+          <div className='stare'>
+            <div className='satre-skills'>
+
+            </div>
+            <div className='satre-skills'>
+
+            </div>
+            <div className='satre-skills'>
+
+            </div>
+            <div className='satre-skills'>
+
+            
+          </div>
+          </div>
+          
+</div>
+</div>
+
+
+        </div>
+        <div className='div'>
+        <div>
+          <div className='contaner2-text'>
+            <h4>Zapier</h4>
+            <p>Organize your life with the Agile Method</p>
+          </div>
+          <div className='contaner2-text'>
+            <h4>Spoke</h4>
+            <p>Jessica Greene Marketing. Freelance Writer</p>
+          </div>
+          <div className='contaner2-text'>
+            <h4>nDash</h4>
+            <p>Why Generalist Writers Lead to Last Profils in B2B tech</p>
+          </div>
+        </div>
+          
+        </div>
+      </div>
+      <div className='contaner1'>
+        <img src='https://tse4.mm.bing.net/th?id=OIP.bJ_A8BrtKZcrC8cX8LgbfwHaJN&pid=Api&P=0&h=220'></img>
+        <img src='https://tse4.mm.bing.net/th?id=OIP.Bchk5OrA3a-SFnmE5q1bpAHaHa&pid=Api&P=0&h=220'></img>
+        <img src='https://tse3.mm.bing.net/th?id=OIP.NGvIyXbgPTCC5MBxdGBHXgHaHC&pid=Api&P=0&h=220'></img>
+        <img src='https://tse4.mm.bing.net/th?id=OIP.bJ_A8BrtKZcrC8cX8LgbfwHaJN&pid=Api&P=0&h=220'></img>
+        
+        
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
